459f1f800cf541707f9d7a494cc3c202a0941686
shaoxian he <solaris.he@icloud.com>
2024-01-18 18:13:36 +1100

    change scroll trigger position

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import { triggerTypeWriter } from './animation/typewriter.js';
 import { enableProjectChange } from './helper/change-project.js';
-
-const bottomVisible = () =>
-  document.documentElement.clientHeight + window.scrollY >=
-  (document.documentElement.scrollHeight ||
-    document.documentElement.clientHeight);
+import * as es from './helper/element-selector.js'
 
 const bgImg = document.querySelector(".background-image");
 
-document.addEventListener("scroll", (e) => {
-    if (bottomVisible()) {
+window.addEventListener("scroll", (event) => {
+    const scroll = window.scrollY;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    const triggerPosition = (scrollHeight - clientHeight) * 0.7;
+    if (scroll >= triggerPosition) {
         bgImg.classList.add("noblur");
     } else {
         bgImg.classList.remove("noblur");
     }
-})
+});
 
 // trigger animation
 window.onload = triggerTypeWriter();
